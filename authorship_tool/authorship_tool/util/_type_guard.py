a85a7df2b71162c76718cc7310a200fe8e1cfd9b
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-11-07 02:33:15 +0900

    ✨ パラグラフのタイプガードを追加

--- authorship_tool/authorship_tool/util/_type_guard.py
+++ authorship_tool/authorship_tool/util/_type_guard.py
@@ -6,7 +6,7 @@ class TypeGuardUtil:
     """タイプガード用のユーティリティクラス"""
 
     @classmethod
-    def is_str_list(cls, values: list, /) -> TypeGuard[list[str]]:
+    def is_sentence(cls, values: list, /) -> TypeGuard[list[str]]:
         """strのリストであることを確認する
 
         Args:
@@ -15,7 +15,23 @@ class TypeGuardUtil:
         Returns:
             TypeGuard[list[str]]: タイプガードされたリスト
         """
-        return isinstance(values, list) and all(isinstance(s, str) for s in values)
+        return isinstance(values, list) and all(
+            isinstance(string, str) for string in values
+        )
+
+    @classmethod
+    def is_paragraph(cls, values: list, /) -> TypeGuard[list[list[str]]]:
+        """strのリストのリストであることを確認する
+
+        Args:
+            values (list): リスト
+
+        Returns:
+            TypeGuard[list[list[str]]]: タイプガードされたリスト
+        """
+        return isinstance(values, list) and all(
+            cls.is_sentence(cent) for cent in values
+        )
 
     @classmethod
     def is_pos_list(cls, values: list, /) -> TypeGuard[list[tuple[str, str]]]:
