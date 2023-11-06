99858b93e299d2ccb1ede87d7e03e750e1e1c66b
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-11-07 06:08:50 +0900

    ✨ 段落とタグ付き文章のタイプガードを追加

--- authorship_tool/authorship_tool/util/_type_guard.py
+++ authorship_tool/authorship_tool/util/_type_guard.py
@@ -32,6 +32,18 @@ class TypeGuardUtil:
         return isinstance(values, list) and all(cls.are_tokens(cent) for cent in values)
 
     @classmethod
+    def are_paras(cls, values: list, /) -> TypeGuard[list[list[list[str]]]]:
+        """strのリストのリストのリストであることを確認する
+
+        Args:
+            values (list): リスト
+
+        Returns:
+            TypeGuard[list[list[list[str]]]]: タイプガードされたリスト
+        """
+        return isinstance(values, list) and all(cls.are_sents(para) for para in values)
+
+    @classmethod
     def are_tagged_tokens(cls, values: list, /) -> TypeGuard[list[tuple[str, str]]]:
         """posのリストであることを確認する
 
@@ -46,4 +58,20 @@ class TypeGuardUtil:
             and all(isinstance(s, str) for s in tpl)
             and len(tpl) == 2
             for tpl in values
+        )
+
+    @classmethod
+    def are_tagged_sents(
+        cls, values: list, /
+    ) -> TypeGuard[list[list[tuple[str, str]]]]:
+        """posのリストのリストであることを確認する
+
+        Args:
+            values (list): リスト
+
+        Returns:
+            TypeGuard[list[list[tuple[str, str]]]]: タイプガードされたリスト
+        """
+        return isinstance(values, list) and all(
+            cls.are_tagged_tokens(pos_list) for pos_list in values
         )
