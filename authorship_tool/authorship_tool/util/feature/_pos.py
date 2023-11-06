65d439b3e282459bbfe15005c1d14c677fcc11a4
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-11-07 02:29:02 +0900

    ✨ PosFeatureにPOSのセット取得機能やJJ_ppデータセット初期化機能を追加

--- authorship_tool/authorship_tool/util/feature/_pos.py
+++ authorship_tool/authorship_tool/util/feature/_pos.py
@@ -47,36 +47,78 @@ class PosFeature:
         """
         return self.__words_and_pos
 
-    def subcategory(self) -> "PosFeature":
+    @property
+    def pos_set(self) -> set[str]:
+        """POSタグの集合を返す
+
+        Returns:
+            set[str]: POSタグの集合
+        """
+        return {pos for (_, pos) in self.__words_and_pos}
+
+    def add_subcategory(self) -> "PosFeature":
         """サブカテゴリを追加する
 
         Returns:
             PosFeature: PosFeatureインスタンス
         """
-        return self.jj_subcategory()
+        return self.add_jj_subcategory()
 
-    def jj_subcategory(self) -> "PosFeature":
+    def add_jj_subcategory(self) -> "PosFeature":
         """形容詞のサブカテゴリを追加する
 
         Returns:
             PosFeature: PosFeatureインスタンス
         """
-        return self.jj_past_participle()
+        if "JJ" not in {pos for (_, pos) in self.__words_and_pos}:
+            return self
 
-    def jj_past_participle(self) -> "PosFeature":
+        return self.add_jj_past_participle()
+
+    def add_jj_past_participle(self) -> "PosFeature":
         """過去分詞形の形容詞を追加する
 
         Returns:
             PosFeature: PosFeatureインスタンス
         """
-        if PAST_PARTICIPLE_ADJECTIVES is None:
-            return self
-
         return PosFeature(
             [
                 (word, "JJ_pp")
-                if word in PAST_PARTICIPLE_ADJECTIVES and pos == "JJ"
+                if word in self.__PAST_PARTICIPLE_ADJECTIVE_DATASET and pos == "JJ"
                 else (word, pos)
                 for (word, pos) in self.__words_and_pos
             ]
         )
+
+    @classmethod
+    def initialize_past_participle_adjective_dataset(cls) -> None:
+        if (project_root_path := PathUtil.PROJECT_ROOT_PATH) is None:
+            print(
+                "Path: $PROJECT_ROOT_PATH could not be found. Skip and continue processing."
+            )
+            return
+
+        dataset_path: str | None = os.getenv("path_adjective_past_participle_dataset")
+        adjectives_past_participle_path: Final[Path | None] = (
+            project_root_path.joinpath(dataset_path)
+            if project_root_path.exists() and dataset_path
+            else None
+        )
+
+        if (
+            adjectives_past_participle_path is None
+            or not adjectives_past_participle_path.exists()
+        ):
+            print(
+                f"File: '{adjectives_past_participle_path}' could not be found. Skip and continue processing."
+            )
+            return
+
+        with open(adjectives_past_participle_path, "r", encoding="utf-8") as f:
+            adjectives: set[str] = set(f.read().splitlines())
+            PosFeature.__PAST_PARTICIPLE_ADJECTIVE_DATASET = sorted(
+                {adj.strip() for adj in adjectives}
+            )
+
+
+PosFeature.initialize_past_participle_adjective_dataset()
