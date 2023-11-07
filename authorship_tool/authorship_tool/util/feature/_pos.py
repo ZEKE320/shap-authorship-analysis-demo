103688a2a61569a0466a05e88b26846de17cb208
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-11-08 01:47:37 +0900

    💄 ハイライトを改善

--- authorship_tool/authorship_tool/util/feature/_pos.py
+++ authorship_tool/authorship_tool/util/feature/_pos.py
@@ -65,7 +65,7 @@ class PosFeature:
         colored_text: str = ""
         for idx, (word, pos) in enumerate(self.__words_and_pos):
             if pos in self.__POS_SUBCATEGORIES:
-                colored_text += f"[bold #5F9EA1]{word}[/bold #5F9EA1]"
+                colored_text += f"[bold #393939 #749BC2 reverse]{word}[/bold #393939 #749BC2 reverse]"
             else:
                 colored_text += f"{word}"
 
