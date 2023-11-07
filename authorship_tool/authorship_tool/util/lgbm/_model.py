a0cc207514222389132880705a950ee111cc7ca3
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-11-07 17:51:21 +0900

    🚚 lgbmモジュールを移動

--- authorship_tool/authorship_tool/util/lgbm/_model.py
+++ authorship_tool/authorship_tool/util/lgbm/_model.py
@@ -1,23 +1,13 @@
-import os
 import pickle
-from os import makedirs, path
+from os import makedirs
 from typing import Final
 
 import pandas as pd
-from dotenv import load_dotenv
 from lightgbm import LGBMClassifier
 from numpy import ndarray
 from pandas import DataFrame
 
-load_dotenv()
-MODEL_PATH: Final[str] = path.join(
-    path.dirname(path.abspath(".env")),
-    os.getenv("path_lgbm_model") or "",
-)
-DATASET_PATH: Final[str] = path.join(
-    path.dirname(path.abspath(".env")),
-    os.getenv("path_dataset") or "",
-)
+from authorship_tool.util import PathUtil
 
 
 class LGBMSourceModel:
@@ -75,25 +65,31 @@ class LGBMResultModel:
         return self.__AUC_ROC_SCORE
 
     def dump(self) -> None:
-        makedirs(MODEL_PATH, exist_ok=True)
-        makedirs(DATASET_PATH, exist_ok=True)
+        makedirs(PathUtil.LGBM_MODEL_PATH, exist_ok=True)
+        makedirs(PathUtil.DATASET_PATH, exist_ok=True)
 
-        with open(path.join(MODEL_PATH, "lgbm_model.pkl"), "wb") as f:
+        with open(PathUtil.LGBM_MODEL_PATH.joinpath("lgbm_model.pkl"), "wb") as f:
             pickle.dump(self.__MODEL, f)
 
-        self.__TRAIN_DATA.to_csv(path.join(DATASET_PATH, "train_data.csv"), index=False)
-        self.__TEST_DATA.to_csv(path.join(DATASET_PATH, "test_data.csv"), index=False)
+        self.__TRAIN_DATA.to_csv(
+            PathUtil.DATASET_PATH.joinpath("train_data.csv"), index=False
+        )
+        self.__TEST_DATA.to_csv(
+            PathUtil.DATASET_PATH.joinpath("test_data.csv"), index=False
+        )
         DataFrame(self.__TRAIN_ANS).to_csv(
-            path.join(DATASET_PATH, "train_ans.csv"), index=False, header=False
+            PathUtil.DATASET_PATH.joinpath("train_ans.csv"), index=False, header=False
         )
         DataFrame(self.__TEST_ANS).to_csv(
-            path.join(DATASET_PATH, "test_ans.csv"), index=False, header=False
+            PathUtil.DATASET_PATH.joinpath("test_ans.csv"), index=False, header=False
         )
         DataFrame(self.__ANS_PRED_PROB).to_csv(
-            path.join(DATASET_PATH, "and_pred_prob.csv"), index=False, header=False
+            PathUtil.DATASET_PATH.joinpath("and_pred_prob.csv"),
+            index=False,
+            header=False,
         )
         DataFrame(self.__ANS_PRED).to_csv(
-            path.join(DATASET_PATH, "ans_pred.csv"), index=False, header=False
+            PathUtil.DATASET_PATH.joinpath("ans_pred.csv"), index=False, header=False
         )
 
     def pred_crosstab(self) -> DataFrame:
