13420146d21b8a5339f2031a82c8870f309ef94f
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-11-07 17:54:15 +0900

    ✨ shapのプロットをsvgで保存する機能を追加

--- authorship_tool/authorship_tool/nltk_gutenberg_analysis.py
+++ authorship_tool/authorship_tool/nltk_gutenberg_analysis.py
@@ -1,40 +1,32 @@
 """NLTKのGutenbergコーパスを用いた著者判定の分析を行うモジュール"""
 # %%
-import os
 import re
-from pathlib import Path
-from os import path
 from typing import Final
 
 import nltk
 import numpy as np
 import shap
 from pandas import DataFrame
-from dotenv import load_dotenv
 from nltk.corpus import gutenberg
+import matplotlib.pyplot as plt
 
-from authorship_tool.lgbm.model import LGBMResultModel, LGBMSourceModel
-from authorship_tool.lgbm.trainer import learn_until_succeed
 from authorship_tool.util import (
+    LGBMResultModel,
+    LGBMSourceModel,
     ArrayDimensionReshaper,
     FeatureDatasetGenerator,
     TabulateUtil,
     PosFeature,
     TypeGuardUtil,
     PathUtil,
+    LGBMTrainerUtil,
 )
-
-load_dotenv()
-DATASET_PATH: Final[Path] = Path(PathUtil.PROJECT_ROOT_PATH or "").joinpath(
-    os.getenv("path.dataset") or ""
-)
-
 
 nltk.download("gutenberg")
 nltk.download("punkt")
 nltk.download("averaged_perceptron_tagger")
 nltk.download("stopwords")
-print()
+
 # %%
 
 DESIRED_SCORE: Final[float] = 0.88
@@ -43,8 +35,6 @@ AUTHOR_B: Final[str] = "austen"
 
 for idx, file_id in enumerate(gutenberg.fileids()):
     print(f"#{idx+1}\t{file_id}")
-
-print()
 
 # %%
 authors = set()
@@ -71,7 +61,6 @@ paragraph_num_by_author_num: dict[str, int] = dict(
 for idx, item in enumerate(paragraph_num_by_author_num.items()):
     print(f"{idx + 1}:\t{item[0]} - {item[1]} paragraphs")
 
-print()
 
 # %%
 
@@ -106,7 +95,9 @@ all_paras: list[list[list[str]]] = (
 if not all_paras:
     raise TypeError("paras_a or paras_b is not list[list[list[str]]] type.")
 
-pos_set: set[str] = set(tag for tag in PosFeature(all_paras).add_subcategory().pos_set)
+pos_set: set[str] = set(
+    tag for tag in PosFeature(all_paras).classify_subcategories().pos_set
+)
 
 all_pos: list[str] = sorted(pos_set)
 print(all_pos)
@@ -131,47 +122,91 @@ df = DataFrame(data, columns=dataset_generator.columns)
 nd_correctness = np.array(correctness)
 
 TabulateUtil.display(df.head(10))
+
+# %%
 print(df.shape)
+# %%
 print(df.dtypes)
+# %%
 print(df.isna().sum())
 
 # %%
-result: LGBMResultModel = learn_until_succeed(
+result: LGBMResultModel = LGBMTrainerUtil.learn_until_succeed(
     LGBMSourceModel(DESIRED_SCORE, df, nd_correctness)
 )
 
-print(f"auc-roc score: {result.auc_roc_score}")
-TabulateUtil.display(result.pred_crosstab())
+
 # %%
+print(f"auc-roc score: {result.auc_roc_score}")
 
 
+# %%
+TabulateUtil.display(result.pred_crosstab())
+
+# %%
 result.dump()
-
 
 explainer = shap.explainers.TreeExplainer(result.model)
 test_shap_val = explainer.shap_values(result.test_data)[1]
 
-
 DataFrame(test_shap_val).to_csv(
-    DATASET_PATH.joinpath("test_shap_val.csv"), index=False, header=False
+    PathUtil.DATASET_PATH.joinpath("test_shap_val.csv"), index=False, header=False
 )
 
-
+# %%
 shap.initjs()
+
+# %%
+shap.force_plot(
+    explainer.expected_value[1],
+    test_shap_val[0],
+    result.test_data.iloc[0],
+)
 shap.force_plot(
     explainer.expected_value[1],
     test_shap_val[0],
     result.test_data.iloc[0],
     matplotlib=True,
+    show=False,
+)
+plt.savefig(
+    PathUtil.SHAP_FIGURE_PATH.joinpath("shap_force_plot.svg"), bbox_inches="tight"
 )
 
-
+# %%
 shap.decision_plot(
-    explainer.expected_value[1], test_shap_val[0], result.test_data.iloc[0]
+    explainer.expected_value[1],
+    test_shap_val[0],
+    result.test_data.iloc[0],
+    show=False,
 )
+plt.savefig(
+    PathUtil.SHAP_FIGURE_PATH.joinpath("shap_decision_plot.svg"), bbox_inches="tight"
+)
+plt.show()
 
 
-shap.summary_plot(test_shap_val, result.test_data)
+# %%
+shap.summary_plot(
+    test_shap_val,
+    result.test_data,
+    show=False,
+)
+plt.savefig(
+    PathUtil.SHAP_FIGURE_PATH.joinpath("shap_summary_plot.svg"), bbox_inches="tight"
+)
+plt.show()
 
+# %%
+shap.summary_plot(
+    test_shap_val,
+    result.test_data,
+    plot_type="bar",
+    show=False,
+)
+plt.savefig(
+    PathUtil.SHAP_FIGURE_PATH.joinpath("shap_summary_plot_bar.svg"), bbox_inches="tight"
+)
+plt.show()
 
-shap.summary_plot(test_shap_val, result.test_data, plot_type="bar")
+# %%
