482a35d3cc37ccc88ce4dfb54fcb3d80c387b682
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-11-11 22:56:12 +0900

    ✨ タグ付けされたトークンのTypeGuardを追加

--- authorship_tool/authorship_tool/util/_type_guard.py
+++ authorship_tool/authorship_tool/util/_type_guard.py
@@ -8,7 +8,7 @@ class TypeGuardUtil:
     """タイプガード用のユーティリティクラス"""
 
     @classmethod
-    def are_tokens(cls, values: list, /) -> TypeGuard[list[str]]:
+    def is_sent(cls, values: list, /) -> TypeGuard[Sent]:
         """strのリストであることを確認する
 
         Args:
@@ -22,7 +22,7 @@ class TypeGuardUtil:
         )
 
     @classmethod
-    def are_sents(cls, values: list, /) -> TypeGuard[list[list[str]]]:
+    def is_para(cls, values: list, /) -> TypeGuard[Para]:
         """strのリストのリストであることを確認する
 
         Args:
@@ -31,10 +31,10 @@ class TypeGuardUtil:
         Returns:
             TypeGuard[list[list[str]]]: タイプガードされたリスト
         """
-        return isinstance(values, list) and all(cls.are_tokens(cent) for cent in values)
+        return isinstance(values, list) and all(cls.is_sent(cent) for cent in values)
 
     @classmethod
-    def are_paras(cls, values: list, /) -> TypeGuard[list[list[list[str]]]]:
+    def are_paras(cls, values: list, /) -> TypeGuard[list[Para]]:
         """strのリストのリストのリストであることを確認する
 
         Args:
@@ -43,10 +43,27 @@ class TypeGuardUtil:
         Returns:
             TypeGuard[list[list[list[str]]]]: タイプガードされたリスト
         """
-        return isinstance(values, list) and all(cls.are_sents(para) for para in values)
+        return isinstance(values, list) and all(cls.is_para(para) for para in values)
 
     @classmethod
-    def are_tagged_tokens(cls, values: list, /) -> TypeGuard[list[tuple[str, str]]]:
+    def is_tagged_token(cls, values: tuple, /) -> TypeGuard[TaggedToken]:
+        """posのタプルであることを確認する
+
+        Args:
+            values (tuple): タプル
+
+        Returns:
+            TypeGuard[tuple[str, str]]: タイプガードされたタプル
+        """
+        return (
+            isinstance(values, tuple)
+            and len(values) == 2
+            and isinstance(values[0], Token)
+            and isinstance(values[1], Tag)
+        )
+
+    @classmethod
+    def are_tagged_tokens(cls, values: list, /) -> TypeGuard[list[TaggedToken]]:
         """posのリストであることを確認する
 
         Args:
@@ -56,16 +73,11 @@ class TypeGuardUtil:
             TypeGuard[list[tuple[str, str]]]: タイプガードされたリスト
         """
         return isinstance(values, list) and all(
-            isinstance(tpl, tuple)
-            and all(isinstance(s, str) for s in tpl)
-            and len(tpl) == 2
-            for tpl in values
+            cls.is_tagged_token(tpl) for tpl in values
         )
 
     @classmethod
-    def are_tagged_sents(
-        cls, values: list, /
-    ) -> TypeGuard[list[list[tuple[str, str]]]]:
+    def are_tagged_sents(cls, values: list, /) -> TypeGuard[list[list[TaggedToken]]]:
         """posのリストのリストであることを確認する
 
         Args:
@@ -77,3 +89,15 @@ class TypeGuardUtil:
         return isinstance(values, list) and all(
             cls.are_tagged_tokens(pos_list) for pos_list in values
         )
+
+    @classmethod
+    def is_tag_set(cls, values: set, /) -> TypeGuard[set[Tag]]:
+        """posのセットであることを確認する
+
+        Args:
+            values (set): セット
+
+        Returns:
+            TypeGuard[set[str]]: タイプガードされたセット
+        """
+        return isinstance(values, set) and all(isinstance(s, Tag) for s in values)
