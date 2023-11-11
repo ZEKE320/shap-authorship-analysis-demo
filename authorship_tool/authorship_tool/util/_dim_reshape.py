43dcf9a02e9572d19008c85f15d56e6ebd12347d
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-11-11 22:36:22 +0900

    🎨 listの次元数変換メソッドの引数を位置引数に変更

--- authorship_tool/authorship_tool/util/_dim_reshape.py
+++ authorship_tool/authorship_tool/util/_dim_reshape.py
@@ -5,16 +5,16 @@ class ArrayDimensionReshaper:
     """配列の次元を変換するクラス"""
 
     @classmethod
-    def para2sent(cls, para: list[list[str]]) -> list[str]:
+    def para2sent(cls, para: list[list[str]], /) -> list[str]:
         """段落のリストを文章のリストに変換する"""
         return [word for sent in para for word in sent]
 
     @classmethod
-    def sent2str(cls, sent: list[str]) -> str:
+    def sent2str(cls, sent: list[str], /) -> str:
         """文章のリストを文字列に変換する"""
         return " ".join(sent)
 
     @classmethod
-    def para2str(cls, para: list[list[str]]) -> str:
+    def para2str(cls, para: list[list[str]], /) -> str:
         """段落のリストを文字列に変換する"""
         return cls.sent2str(cls.para2sent(para))
