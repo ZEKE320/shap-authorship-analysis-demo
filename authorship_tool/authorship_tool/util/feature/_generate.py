39dee9b41d757df85afecfde485491ee31bf93b7
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-11-12 00:21:53 +0900

    ♻️ データセット作成時にタグを引数に取らないように変更

--- authorship_tool/authorship_tool/util/feature/_generate.py
+++ authorship_tool/authorship_tool/util/feature/_generate.py
@@ -1,5 +1,5 @@
 """特徴量のデータセットを生成するモジュール"""
-from typing import Optional
+from typing import Callable, Final, Optional
 
 from authorship_tool.util import FeatureCalculator, FeatureCounter, TypeGuardUtil
 from authorship_tool.type_alias import Para, Sent, Tag
@@ -8,9 +8,9 @@ from authorship_tool.type_alias import Para, Sent, Tag
 class FeatureDatasetGenerator:
     """特徴量のデータセットを生成するクラス"""
 
-    def __init__(self, tags: Optional[set[Tag]] = None) -> None:
-        if tags and not TypeGuardUtil.is_tag_set(tags):
-            raise ValueError("tags must be a set of str")
+    def __init__(self, tags: Optional[tuple[Tag, ...]] = None) -> None:
+        if tags and not TypeGuardUtil.is_tag_tuple(tags):
+            raise ValueError("tags must be a tuple of str")
 
         col: list[str] = [
             "word variation",
@@ -20,9 +20,7 @@ class FeatureDatasetGenerator:
         ]
 
         if tags:
-            col.extend(sorted(tags))
-
-        self.__columns: tuple[str, ...] = tuple(col)
+        self.__tags: Final[tuple[Tag, ...]] = tags if tags else tuple()
 
     @property
     def columns(self) -> tuple[str, ...]:
@@ -30,20 +28,14 @@ class FeatureDatasetGenerator:
         return self.__columns
 
     def generate_from_sentence(
-        self, sent: Sent, tags: set[Tag], correctness: bool
+        self, sent: Sent, correctness: bool
     ) -> tuple[tuple[float, ...], bool]:
         """文章のリストから特徴量のリストを生成する"""
         freq_by_pos: dict[str, float] = FeatureCalculator.all_pos_frequency(sent)
 
         return (
             tuple(
-                [
-                    FeatureCalculator.word_variation(sent),
-                    FeatureCalculator.uncommon_word_frequency(sent),
-                    FeatureCounter.sentence_length(sent),
-                    FeatureCalculator.average_word_length(sent),
-                ]
-                + [freq_by_pos.get(tag, 0.0) for tag in tags]
+                + [freq_by_pos.get(tag, 0.0) for tag in self.__tags]
             ),
             correctness,
         )
@@ -51,9 +43,8 @@ class FeatureDatasetGenerator:
     def generate_from_paragraph(
         self,
         para: Para,
-        tags: set[Tag],
         correctness: bool,
     ) -> tuple[tuple[float, ...], bool]:
         """シェイプを落としてから特徴量のリストを生成する"""
         sent: Sent = [word for sent in para for word in sent]
-        return self.generate_from_sentence(sent, tags, correctness)
+        return self.generate_from_sentence(sent, correctness)
