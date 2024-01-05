a8fd4e18795611206855862f8292c58434c1af12
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2024-01-05 16:23:47 +0000

    ✨ configファイルを追加

---
+++ authorship_tool/config.py
@@ -0,0 +1,6 @@
+PATHS = {
+    "path_dump_lgbm_model_dir": "dump/lgbm/model",
+    "path_dump_dataset_dir": "dump/dataset",
+    "path_dump_shap_figure_dir": "dump/shap/figure",
+    "path_adjective_past_participle_dataset": "data/nlp_datasets/wordLists/adjectivesPastParticiple",
+}
