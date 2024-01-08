6b64c494ddc16d506ff3450613add6946367bad2
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2024-01-08 11:33:51 +0900

    ♻️ Refactor Gutenberg and Inaugural analysis

--- demo/nltk_inaugural_analysis.py
+++ demo/nltk_inaugural_analysis.py
@@ -1,15 +1,14 @@
 # %%
-import re
 from os import makedirs
 from typing import Final, TypeAlias
 
-import matplotlib.pyplot as plt
 import nltk
 import numpy as np
 import pandas as pd
 import shap
 from IPython.display import display
-from nltk.corpus import gutenberg
+from matplotlib import pyplot as plt
+from nltk.corpus import inaugural
 from pandas import DataFrame
 
 from authorship_tool.types import Para, Tag
@@ -20,92 +19,85 @@ from authorship_tool.util.lgbm import trainer as lgbm_trainer
 from authorship_tool.util.lgbm.model import LGBMResultModel, LGBMSourceModel
 from authorship_tool.util.path_util import PathUtil
 
-# 必要に応じてダウンロード
-nltk.download("gutenberg")
+nltk.download("inaugural")
 nltk.download("punkt")
 nltk.download("averaged_perceptron_tagger")
 nltk.download("stopwords")
 
 # %%
-AUTHOR_A: Final[str] = "chesterton"
-AUTHOR_B: Final[str] = "austen"
-DESIRED_ROC_SCORE: Final[float] = 0.88
+PRESIDENT_A = "Bush"
+PRESIDENT_B = "Harrison"
+DESIRED_ROC_SCORE: Final[float] = 0.99
 
 
 # %%
-for idx, file_id in enumerate(iterable=gutenberg.fileids()):
+for idx, file_id in enumerate(inaugural.fileids()):
     print(f"#{idx+1}\t{file_id}")
 
 
 # %%
-Author: TypeAlias = str
+President: TypeAlias = str
 NumOfParas: TypeAlias = int
 
-authors: set[Author] = {
-    match.group(1)
-    for file_id in gutenberg.fileids()
-    if (match := re.search(r"^(.+?)-", file_id)) is not None
-}
+presidents: set[President] = {file_id[5:-4] for file_id in inaugural.fileids()}
 
-para_size_by_author: dict[Author, NumOfParas] = {}
+president_data_dict: dict[President, NumOfParas] = {}
 
-for index, author in enumerate(iterable=authors):
-    books: list[list[Para]] = [
-        gutenberg.paras(fileids=file_id)
-        for file_id in gutenberg.fileids()
-        if author in file_id
+for index, president in enumerate(iterable=presidents):
+    speeches: list[list[Para]] = [
+        # inaugural.sents(file_id)
+        inaugural.paras(fileids=file_id)
+        for file_id in inaugural.fileids()
+        if president in file_id
     ]  # type: ignore
 
-    para_num: NumOfParas = len([para for paras in books for para in paras])
-    para_size_by_author[author] = para_num
+    para_num: NumOfParas = len([para for paras in speeches for para in paras])
+    president_data_dict[president] = para_num
 
-sorted_para_size_by_author: dict[Author, NumOfParas] = dict(
-    sorted(para_size_by_author.items(), key=lambda item: item[1], reverse=True)
+sorted_para_size_by_president: dict[President, NumOfParas] = dict(
+    sorted(president_data_dict.items(), key=lambda item: item[1], reverse=True)
 )
 
-for idx, item in enumerate(sorted_para_size_by_author.items()):
+for idx, item in enumerate(sorted_para_size_by_president.items()):
     print(f"{idx + 1}:\t{item[0]} - {item[1]} paragraphs")
 
 
 # %%
-books_a: list[list[Para]] = [
-    gutenberg.paras(fileids=file_id)
-    for file_id in gutenberg.fileids()
-    if AUTHOR_A in file_id
-]  # type: ignore
+speeches_a = [
+    inaugural.paras(file_id)
+    for file_id in inaugural.fileids()
+    if PRESIDENT_A in file_id
+]
 
-paras_a: list[Para] = [para for paras in books_a for para in paras]
+paras_a = [para for paras in speeches_a for para in paras]
 if len(paras_a) == 0 or not type_guard.are_paras(paras_a):
     raise ValueError("paras_a is empty or not list[Para]")
 
 for para in paras_a[:10]:
     print(dim_reshaper.para2str(para))
 
-print(f"...\n\nAuthor: {AUTHOR_A}, {len(paras_a)} paragraphs\n")
+print(f"...\n\nSpeaker: President {PRESIDENT_A}, {len(paras_a)} paragraphs\n")
 
 
 # %%
-
-books_b: list[list[Para]] = [
-    gutenberg.paras(fileids=file_id)
-    for file_id in gutenberg.fileids()
-    if AUTHOR_B in file_id
+speeches_b: list[list[Para]] = [
+    inaugural.paras(file_id)
+    for file_id in inaugural.fileids()
+    if PRESIDENT_B in file_id
 ]  # type: ignore
 
-paras_b: list[Para] = [para for paras in books_b for para in paras]
-if len(paras_b) == 0 or not type_guard.are_paras(paras_b):
-    raise ValueError("paras_a is empty or not list[list[str]]")
-
-for para in paras_b[:10]:
+sents_b: list[Para] = [para for paras in speeches_b for para in paras]
+for para in sents_b[:50]:
     print(dim_reshaper.para2str(para))
 
-print(f"...\n\nAuthor: {AUTHOR_B}, {len(paras_b)} paragraphs\n")
+print(f"...\n\nSpeaker: President {PRESIDENT_B}, {len(sents_b)} paragraphs\n")
 
 
 # %%
-if not (type_guard.are_paras(paras_a) and type_guard.are_paras(paras_b)):
-    raise TypeError("paras_a or paras_b is not list[Para]")
-all_paras: list[Para] = paras_a + paras_b
+
+if not (type_guard.are_paras(paras_a) and type_guard.are_paras(sents_b)):
+    raise ValueError("paras_a or sents_b is not list[Para]")
+all_paras: list[Para] = paras_a + sents_b
 
 pos_list: list[Tag] = PosFeature(all_paras).tag_subcategories().pos_list
 
@@ -113,23 +105,23 @@ print(pos_list)
 
 
 # %%
-dataset_generator = FeatureDatasetGenerator(tags=pos_list)
+dataset_generator = FeatureDatasetGenerator(pos_list)
 data: list[tuple[float, ...]] = []
 correctness: list[bool] = []
 
-for para_a in paras_a:
-    (x, y) = dataset_generator.generate_from_paragraph(para_a, True)
+for para in paras_a:
+    x, y = dataset_generator.generate_from_paragraph(para, True)
     data.append(x)
     correctness.append(y)
 
-for para_b in paras_b:
-    (x, y) = dataset_generator.generate_from_paragraph(para_b, False)
+for para in sents_b:
+    x, y = dataset_generator.generate_from_paragraph(para, False)
     data.append(x)
     correctness.append(y)
 
 
 # %%
-df = DataFrame(data, columns=dataset_generator.columns)
+df = pd.DataFrame(data, columns=dataset_generator.columns)
 nd_correctness = np.array(correctness)
 
 pd.set_option("display.max_columns", 1000)
@@ -164,7 +156,7 @@ display(result.pred_crosstab())
 
 
 # %%
-result.dump("gutenberg")
+result.dump("inaugural")
 
 
 # %%
@@ -172,7 +164,9 @@ explainer = shap.TreeExplainer(result.model)
 test_shap_val = explainer.shap_values(result.test_data)[1]
 
 DataFrame(test_shap_val).to_csv(
-    PathUtil.DATASET_DIR.joinpath("test_shap_val.csv"), index=False, header=False
+    PathUtil.DATASET_DIR.joinpath("inaugural", "test_shap_val.csv"),
+    index=False,
+    header=False,
 )
 
 
@@ -181,7 +175,7 @@ shap.initjs()
 
 
 # %%
-makedirs(PathUtil.SHAP_FIGURE_DIR.joinpath("gutenberg"), exist_ok=True)
+makedirs(PathUtil.SHAP_FIGURE_DIR.joinpath("inaugural"), exist_ok=True)
 
 # %%
 shap.force_plot(
@@ -197,7 +191,7 @@ shap.force_plot(
     show=False,
 )
 plt.savefig(
-    PathUtil.SHAP_FIGURE_DIR.joinpath("gutenberg", "shap_force_plot.svg"),
+    PathUtil.SHAP_FIGURE_DIR.joinpath("inaugural", "shap_force_plot.svg"),
     bbox_inches="tight",
 )
 plt.show()
@@ -212,7 +206,7 @@ shap.decision_plot(
     show=False,
 )
 plt.savefig(
-    PathUtil.SHAP_FIGURE_DIR.joinpath("gutenberg", "shap_decision_plot.svg"),
+    PathUtil.SHAP_FIGURE_DIR.joinpath("inaugural", "shap_decision_plot.svg"),
     bbox_inches="tight",
 )
 plt.show()
@@ -226,7 +220,7 @@ shap.summary_plot(
     show=False,
 )
 plt.savefig(
-    PathUtil.SHAP_FIGURE_DIR.joinpath("gutenberg", "shap_summary_plot.svg"),
+    PathUtil.SHAP_FIGURE_DIR.joinpath("inaugural", "shap_summary_plot.svg"),
     bbox_inches="tight",
 )
 plt.show()
@@ -241,7 +235,7 @@ shap.summary_plot(
     show=False,
 )
 plt.savefig(
-    PathUtil.SHAP_FIGURE_DIR.joinpath("gutenberg", "shap_summary_plot_bar.svg"),
+    PathUtil.SHAP_FIGURE_DIR.joinpath("inaugural", "shap_summary_plot_bar.svg"),
     bbox_inches="tight",
 )
 plt.show()
