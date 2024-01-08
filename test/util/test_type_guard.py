3ac15fc58ec914e1eb8a6d9ac4fb8d7021d697c7
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2024-01-08 11:34:27 +0900

    ✅ Refactor type guard tests

--- test/util/test_type_guard.py
+++ test/util/test_type_guard.py
@@ -2,31 +2,31 @@
 import nltk
 import pytest
 
-from authorship_tool.util import TypeGuardUtil
+from authorship_tool.util import type_guard
 
 
-def test_is_str_list() -> None:
+def test_is_sent() -> None:
     """strのリストであることを確認できること"""
-    assert TypeGuardUtil.is_str_list([])
-    assert TypeGuardUtil.is_str_list([""])
-    assert TypeGuardUtil.is_str_list(["a", "b"])
-    assert not TypeGuardUtil.is_str_list(["a", 1])
-    assert not TypeGuardUtil.is_str_list([1, 2])
-    assert not TypeGuardUtil.is_str_list("a")  # type: ignore
-    assert not TypeGuardUtil.is_str_list(1)  # type: ignore
+    assert type_guard.is_sent([])
+    assert type_guard.is_sent([""])
+    assert type_guard.is_sent(["a", "b"])
+    assert not type_guard.is_sent(["a", 1])
+    assert not type_guard.is_sent([1, 2])
+    assert not type_guard.is_sent("a")  # type: ignore
+    assert not type_guard.is_sent(1)  # type: ignore
 
 
 def test_is_pos_list() -> None:
     """posのリストであることを確認できること"""
-    assert TypeGuardUtil.are_tagged_tokens([])
-    assert TypeGuardUtil.are_tagged_tokens([("", "")])
-    assert TypeGuardUtil.are_tagged_tokens([("a", "b"), ("c", "d")])
-    assert not TypeGuardUtil.are_tagged_tokens([("a", 1)])
-    assert not TypeGuardUtil.are_tagged_tokens([("a", "b", "c")])
-    assert not TypeGuardUtil.are_tagged_tokens("a")  # type: ignore
-    assert not TypeGuardUtil.are_tagged_tokens(1)  # type: ignore
+    assert type_guard.are_tagged_tokens([])
+    assert type_guard.are_tagged_tokens([("", "")])
+    assert type_guard.are_tagged_tokens([("a", "b"), ("c", "d")])
+    assert not type_guard.are_tagged_tokens([("a", 1)])
+    assert not type_guard.are_tagged_tokens([("a", "b", "c")])
+    assert not type_guard.are_tagged_tokens("a")  # type: ignore
+    assert not type_guard.are_tagged_tokens(1)  # type: ignore
 
-    assert TypeGuardUtil.are_tagged_tokens(nltk.pos_tag(["She", "looks", "busy", "."]))
+    assert type_guard.are_tagged_tokens(nltk.pos_tag(["He", "looks", "busy", "."]))
 
 
 if __name__ == "__main__":
