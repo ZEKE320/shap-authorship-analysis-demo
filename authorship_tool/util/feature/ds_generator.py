8942f9c137ba0803860754d504237ae89bc2aeeb
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2024-01-09 04:59:48 +0000

    ✨ Add ParagraphFeatureDatasetGenerator class for generating paragraph feature datasets

--- authorship_tool/util/feature/ds_generator.py
+++ authorship_tool/util/feature/ds_generator.py
@@ -65,3 +65,64 @@ class SentenceFeatureDatasetGenerator:
         return self.generate_from_sentence(sent, correctness)
 
 
+class ParagraphFeatureDatasetGenerator:
+    """
+    段落特徴量データセット生成クラス
+    Paragraph feature dataset generator class
+    """
+
+    __COLS_AND_FUNC: Final[dict[str, Callable[[Para2dStr], float | int]]] = {
+        "v1 sentences per paragraph": UnivKansasFeatures.v1_sentences_per_paragraph,
+        "v2 words per paragraph": UnivKansasFeatures.v2_words_per_paragraph,
+        "v3 close parenthesis present": UnivKansasFeatures.v3_close_parenthesis_present,
+        "v4 dash present": UnivKansasFeatures.v4_dash_present,
+        "v5 semi-colon or colon present": UnivKansasFeatures.v5_semi_colon_or_colon_present,
+        "v6 question mark present": UnivKansasFeatures.v6_question_mark_present,
+        "v7 apostrophe present": UnivKansasFeatures.v7_apostrophe_present,
+        "v8 standard deviation of sentence length": UnivKansasFeatures.v8_standard_deviation_of_sentence_length,
+        "v9 length difference for consecutive sentences": UnivKansasFeatures.v9_length_difference_for_consecutive_sentences,
+        "v10 sentence with < 11 words": UnivKansasFeatures.v10_sentence_with_lt_11_words,
+        "v11 sentence with > 34 words": UnivKansasFeatures.v11_sentence_with_gt_34_words,
+        "v12 contains although": UnivKansasFeatures.v12_contains_although,
+        "v13 contains however": UnivKansasFeatures.v13_contains_however,
+        "v14 contains but": UnivKansasFeatures.v14_contains_but,
+        "v15 contains because": UnivKansasFeatures.v15_contains_because,
+        "v16 contains this": UnivKansasFeatures.v16_contains_this,
+        "v17 contains others or researchers": UnivKansasFeatures.v17_contains_others_or_researchers,
+        "v18 contains numbers": UnivKansasFeatures.v18_contains_numbers,
+        "v19 contains 2 times more capitals than period": UnivKansasFeatures.v19_contains_2_times_more_capitals_than_period,
+        "v20 contains et": UnivKansasFeatures.v20_contains_et,
+    }
+
+    def __init__(self, tags: Optional[list[Tag]] = None) -> None:
+        if tags and not type_guard.is_tag_list(tags):
+            raise ValueError("tags must be a list of str")
+
+        col: list[str] = list(ParagraphFeatureDatasetGenerator.__COLS_AND_FUNC.keys())
+        if tags:
+            col.extend(tags)
+
+        # クラスのフィールドを定義
+        self.__columns: Final[tuple[str, ...]] = tuple(col)
+        self.__tags: Final[list[Tag]] = tags if tags else []
+
+    @property
+    def columns(self) -> tuple[str, ...]:
+        """特徴量の列名"""
+        return self.__columns
+
+    def generate_from_paragraph(
+        self,
+        para: Para2dStr,
+        correctness: bool,
+    ) -> tuple[tuple[float, ...], bool]:
+        """文字列のリストのリストから特徴量のリストを生成する"""
+        freq_by_pos: dict[str, float] = ParagraphCalculator.pos_frequencies(para)
+
+        return (
+            tuple(
+                [func(para) for func in self.__COLS_AND_FUNC.values()]
+                + [freq_by_pos.get(tag, 0.0) for tag in self.__tags]
+            ),
+            correctness,
+        )
