22891408ad1505da1b69dd0c52f08c4549e3a125
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2024-01-10 21:22:40 +0000

    ♻️ Refactor model.py data classes and add dump and pred_crosstab functions

--- authorship_tool/util/ml/model.py
+++ authorship_tool/util/ml/model.py
@@ -1,12 +1,13 @@
 """LGBMに利用するモデルデータ定義モジュール"""
 import pickle
 from pathlib import Path
-from typing import Final, Optional
+from typing import Final, TypedDict
 
+import numpy as np
 import pandas as pd
 from attr import dataclass
 from lightgbm import LGBMClassifier
-from numpy import ndarray
+from numpy.typing import NDArray
 from pandas import DataFrame
 from shap import Explainer
 
@@ -14,122 +15,133 @@ from authorship_tool.util.path_util import PathUtil
 
 
 @dataclass(frozen=True)
-class LGBMSource:
+class LGBMSource(TypedDict):
     """LGBMのモデル作成用データクラス"""
 
     feature_data_frame: DataFrame
-    nd_category: ndarray
+    nd_category: NDArray[np.bool_]
 
 
 @dataclass(frozen=True)
-class SplittedDataset:
+class SplittedDataset(TypedDict):
     train_data: DataFrame
     test_data: DataFrame
-    train_ans: ndarray
-    test_ans: ndarray
+    train_ans: NDArray
+    test_ans: NDArray
 
 
 @dataclass(frozen=True)
-class Prediction:
-    pred_prob: ndarray
-    pred_ans: ndarray
+class Prediction(TypedDict):
+    pred_prob: NDArray
+    pred_ans: NDArray
 
 
 @dataclass(frozen=True)
-class Score:
-    f1_score: float
-    accuracy_score: float
-    auc_roc_score: Optional[float] = None
+class Score(TypedDict):
+    f1_score: np.float64 | NDArray[np.float64]
+    accuracy_score: np.float64
+    auc_roc_score: np.float64 | None
 
 
 @dataclass(frozen=True)
-class ShapData:
+class ShapData(TypedDict):
     explainer: Explainer
-    shap_positive_vals: ndarray
-    shap_positive_expected_val: float
+    shap_positive_vals: NDArray
+    shap_positive_expected_val: np.float64
 
 
 @dataclass(frozen=True)
-class LGBMResult:
+class LGBMResult(TypedDict):
     """LGBMのモデル作成結果データクラス"""
 
     model: LGBMClassifier
     splitted_dataset: SplittedDataset
     prediction: Prediction
     shap_data: ShapData
-    score: Optional[Score] = None
-
-    def dump(self, title: Optional[str] = None) -> None:
-        """
-        作成したモデル、データを保存する
-
-        Args:
-            title (Optional[str]): フォルダ名に使用するタイトル
-        """
-        if title is None:
-            title = "_output_"
-
-        LGBM_MODEL_DIR: Final[Path] = PathUtil.LGBM_MODEL_DIR.joinpath(title)
-        DATASET_DIR: Final[Path] = PathUtil.DATASET_DIR.joinpath(title)
-
-        LGBM_MODEL_DIR.mkdir(exist_ok=True)
-        LGBM_MODEL_DIR.mkdir(exist_ok=True)
-
-        with open(LGBM_MODEL_DIR.joinpath("lgbm_model.pkl"), "wb") as f:
-            pickle.dump(self.model, f)
-
-        self.splitted_dataset.train_data.to_csv(
-            DATASET_DIR.joinpath("train_data.csv"), index=False
-        )
-        self.splitted_dataset.test_data.to_csv(
-            DATASET_DIR.joinpath("test_data.csv"), index=False
-        )
-        DataFrame(self.splitted_dataset.train_ans).to_csv(
-            DATASET_DIR.joinpath("train_ans.csv"),
-            index=False,
-            header=False,
-        )
-        DataFrame(self.splitted_dataset.test_ans).to_csv(
-            DATASET_DIR.joinpath("test_ans.csv"),
-            index=False,
-            header=False,
-        )
-
-        DataFrame(self.prediction.pred_prob).to_csv(
-            DATASET_DIR.joinpath("ans_pred_prob.csv"),
-            index=False,
-            header=False,
-        )
-        DataFrame(self.prediction.pred_ans).to_csv(
-            DATASET_DIR.joinpath("ans_pred.csv"),
-            index=False,
-            header=False,
-        )
-
-        DataFrame(self.shap_data.shap_positive_vals).to_csv(
-            DATASET_DIR.joinpath("test_shap_val.csv"),
-            index=False,
-            header=False,
-        )
-
-    def pred_crosstab(self) -> DataFrame:
-        """
-        予測結果のクロス集計を行う
-
-        Returns:
-            DataFrame: クロス集計結果
-        """
-        return pd.crosstab(
-            self.splitted_dataset.test_ans,
-            self.prediction.pred_ans,
-            rownames=["actual"],
-            colnames=["predicted"],
-        )
+    score: Score | None
 
 
 @dataclass(frozen=True)
-class LGBMCvResult:
+class LGBMCvResult(TypedDict):
     models: list[LGBMClassifier]
     splitted_datasets: list[SplittedDataset]
     predictions: list[Prediction]
     shap_data_list: list[ShapData]
+
+
+@dataclass(frozen=True)
+class CvViewData(TypedDict):
+    test_ans: DataFrame
+    pred_ans: DataFrame
+    pred_prob: DataFrame
+    shap_positive_vals: DataFrame
+    shap_expected_val: np.float64
+
+
+def dump(result: LGBMResult, title: str | None = None) -> None:
+    """
+    作成したモデル、データを保存する
+
+    Args:
+        title (Optional[str]): フォルダ名に使用するタイトル
+    """
+    if title is None:
+        title = "_output_"
+
+    LGBM_MODEL_DIR: Final[Path] = PathUtil.LGBM_MODEL_DIR.joinpath(title)
+    DATASET_DIR: Final[Path] = PathUtil.DATASET_DIR.joinpath(title)
+
+    LGBM_MODEL_DIR.mkdir(exist_ok=True)
+    LGBM_MODEL_DIR.mkdir(exist_ok=True)
+
+    with open(LGBM_MODEL_DIR.joinpath("lgbm_model.pkl"), "wb") as f:
+        pickle.dump(result["model"], f)
+
+    result["splitted_dataset"]["train_data"].to_csv(
+        DATASET_DIR.joinpath("train_data.csv"), index=False
+    )
+    result["splitted_dataset"]["test_data"].to_csv(
+        DATASET_DIR.joinpath("test_data.csv"), index=False
+    )
+    DataFrame(result["splitted_dataset"]["train_ans"]).to_csv(
+        DATASET_DIR.joinpath("train_ans.csv"),
+        index=False,
+        header=False,
+    )
+    DataFrame(result["splitted_dataset"]["test_ans"]).to_csv(
+        DATASET_DIR.joinpath("test_ans.csv"),
+        index=False,
+        header=False,
+    )
+
+    DataFrame(result["prediction"]["pred_prob"]).to_csv(
+        DATASET_DIR.joinpath("ans_pred_prob.csv"),
+        index=False,
+        header=False,
+    )
+    DataFrame(result["prediction"]["pred_ans"]).to_csv(
+        DATASET_DIR.joinpath("ans_pred.csv"),
+        index=False,
+        header=False,
+    )
+
+    DataFrame(result["shap_data"]["shap_positive_vals"]).to_csv(
+        DATASET_DIR.joinpath("test_shap_val.csv"),
+        index=False,
+        header=False,
+    )
+
+
+def pred_crosstab(result: LGBMResult) -> DataFrame:
+    """
+    予測結果のクロス集計を行う
+
+    Returns:
+        DataFrame: クロス集計結果
+    """
+    return pd.crosstab(
+        result["splitted_dataset"]["test_ans"],
+        result["prediction"]["pred_ans"],
+        rownames=["actual"],
+        colnames=["predicted"],
+    )
--- authorship_tool/util/ml/trainer.py
+++ authorship_tool/util/ml/trainer.py
@@ -1,10 +1,11 @@
 """lightgbmトレーナー"""
 
-from typing import Final, Optional
+from typing import Final
 
+import numpy as np
 import shap
 from lightgbm import LGBMClassifier
-from numpy import ndarray
+from numpy.typing import NDArray
 from pandas import DataFrame
 from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
 from sklearn.model_selection import LeaveOneOut, train_test_split
@@ -24,50 +25,60 @@ SHAP_VALUE_POSITIVE_IDX: Final[int] = 1
 
 def train(dataset: SplittedDataset, use_score: bool = False) -> LGBMResult:
     model = LGBMClassifier()
-    model.fit(X=dataset.train_data.values, y=dataset.train_ans)
+    model.fit(X=dataset["train_data"].values, y=dataset["train_ans"])
 
-    prediction: Prediction = predict(model, dataset.test_data)
-    shap_result: ShapData = create_shap_data(model, dataset.test_data)
+    prediction: Prediction = predict(model, dataset["test_data"])
+    shap_result: ShapData = create_shap_data(model, dataset["test_data"])
 
-    score: Score | None = calc_score(prediction, dataset.test_ans, use_score)
+    score: Score | None = calc_score(prediction, dataset["test_ans"], use_score)
 
-    return LGBMResult(
-        model,
-        dataset,
-        prediction,
-        shap_result,
-        score if use_score else None,
-    )
+    return {
+        "model": model,
+        "splitted_dataset": dataset,
+        "prediction": prediction,
+        "shap_data": shap_result,
+        "score": score if use_score else None,
+    }
 
 
 def predict(model: LGBMClassifier, test_data: DataFrame) -> Prediction:
-    ans_pred_prob = model.predict_proba(X=test_data)[:, 1]
-    ans_pred = model.predict(X=test_data)
+    pred_prob: NDArray[np.float64] = np.array(
+        model.predict_proba(X=test_data), dtype=np.float64
+    )[:, 1]
+    pred_ans: NDArray[np.bool_] = np.array(model.predict(X=test_data), dtype=np.bool_)
 
-    return Prediction(ans_pred_prob, ans_pred)
+    return {"pred_prob": pred_prob, "pred_ans": pred_ans}
 
 
 def calc_score(
-    prediction: Prediction, test_ans: ndarray, use_score: bool
-) -> Optional[Score]:
+    prediction: Prediction, test_ans: NDArray, use_score: bool
+) -> Score | None:
     if len(test_ans) <= 1 or not use_score:
         return None
 
-    f1_result = f1_score(y_true=test_ans, y_pred=prediction.pred_ans)
-    accuracy_result = accuracy_score(y_true=test_ans, y_pred=prediction.pred_ans)
-    auc_roc_result = roc_auc_score(y_true=test_ans, y_score=prediction.pred_prob)
+    f1_result = f1_score(y_true=test_ans, y_pred=prediction["pred_ans"])
+    accuracy_result = accuracy_score(y_true=test_ans, y_pred=prediction["pred_ans"])
+    auc_roc_result = roc_auc_score(y_true=test_ans, y_score=prediction["pred_prob"])
 
-    return Score(f1_result, accuracy_result, auc_roc_result)
+    return {
+        "f1_score": f1_result,
+        "accuracy_score": accuracy_result,
+        "auc_roc_score": auc_roc_result,
+    }
 
 
 def create_shap_data(model: LGBMClassifier, test_data: DataFrame) -> ShapData:
     tree_explainer = shap.TreeExplainer(model)
     test_shap_val = tree_explainer.shap_values(test_data)[SHAP_VALUE_POSITIVE_IDX]
-    test_shap_expected_val = list(tree_explainer.expected_value)[
+    test_shap_expected_val = np.array(tree_explainer.expected_value)[
         SHAP_VALUE_POSITIVE_IDX
     ]
 
-    return ShapData(tree_explainer, test_shap_val, test_shap_expected_val)
+    return {
+        "explainer": tree_explainer,
+        "shap_positive_vals": test_shap_val,
+        "shap_positive_expected_val": test_shap_expected_val,
+    }
 
 
 def train_once(training_source: LGBMSource) -> LGBMResult:
@@ -83,49 +94,76 @@ def train_once(training_source: LGBMSource) -> LGBMResult:
         テスト用予測確率、テスト用予測ラベル、ROC AUCスコア
     """
 
-    dataset = SplittedDataset(
-        *train_test_split(
-            training_source.feature_data_frame, training_source.nd_category
-        )
+    train_data, test_data, train_ans, test_ans = train_test_split(
+        training_source["feature_data_frame"], training_source["nd_category"]
     )
+
+    dataset: SplittedDataset = {
+        "train_data": train_data,
+        "test_data": test_data,
+        "train_ans": train_ans,
+        "test_ans": test_ans,
+    }
 
     return train(dataset, use_score=True)
 
 
 def train_by_index(
     source: LGBMSource,
-    train_indices: ndarray,
-    test_index: ndarray,
+    train_indices: NDArray,
+    test_index: NDArray,
     use_score: bool = False,
 ) -> LGBMResult:
     X_train, X_test = (
-        source.feature_data_frame.iloc[train_indices],
-        source.feature_data_frame.iloc[test_index],
+        source["feature_data_frame"].iloc[train_indices],
+        source["feature_data_frame"].iloc[test_index],
     )
     y_train, y_test = (
-        source.nd_category[train_indices],
-        source.nd_category[test_index],
+        source["nd_category"][train_indices],
+        source["nd_category"][test_index],
     )
-    splitted_dataset = SplittedDataset(X_train, X_test, y_train, y_test)
+    splitted_dataset: SplittedDataset = {
+        "train_data": X_train,
+        "test_data": X_test,
+        "train_ans": y_train,
+        "test_ans": y_test,
+    }
 
     return train(splitted_dataset, use_score)
 
 
-def loocv_train(source: LGBMSource) -> LGBMCvResult:
+def train_loocv(source: LGBMSource) -> LGBMCvResult:
     loo = LeaveOneOut()
 
     results: list[LGBMResult] = [
         train_by_index(source, train_indices, test_index, use_score=False)
-        for train_indices, test_index in loo.split(source.feature_data_frame)
+        for train_indices, test_index in loo.split(source["feature_data_frame"])
     ]
 
-    # cv_result: LGBMCvResult = LGBMCvResult(
-    #     [result.model for result in results],
-    #     [result.splitted_dataset for result in results],
-    #     [result.prediction for result in results],
-    #     [result.shap_data for result in results],
-    #     None,
-    # )
+    cv_result: LGBMCvResult = convert_results_to_cv_result(results)
 
-    # return cv_result
-    return results
+    return cv_result
+
+
+def convert_results_to_cv_result(results: list[LGBMResult]) -> LGBMCvResult:
+    models, datasets, predictions, shap_data_list = map(
+        list,
+        zip(
+            *(
+                (
+                    result["model"],
+                    result["splitted_dataset"],
+                    result["prediction"],
+                    result["shap_data"],
+                )
+                for result in results
+            )
+        ),
+    )
+
+    return {
+        "models": models,
+        "splitted_datasets": datasets,
+        "predictions": predictions,
+        "shap_data_list": shap_data_list,
+    }
