b8f25f8dbd6da20b1feae5da32ca34c5f9cef15e
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2024-01-13 08:59:55 +0000

    ♻️ Refactor type aliases in types.py

--- authorship_tool/types.py
+++ authorship_tool/types.py
@@ -1,15 +1,24 @@
 """プロジェクト全般の型エイリアス"""
 from typing import TypeAlias
 
-# 文章関連の型エイリアス
+# 文章関連 型エイリアス
 
 Tag: TypeAlias = str
 """タグ"""
+
 TokenStr: TypeAlias = str
 """単語"""
+
 Sent1dStr: TypeAlias = list[TokenStr]
 """文"""
+
 Para2dStr: TypeAlias = list[Sent1dStr]
 """段落"""
+
 TaggedToken: TypeAlias = tuple[TokenStr, Tag]
 """タグ付けされた単語"""
+
+# 特徴量関連 型エイリアス
+
+FeatureLabel: TypeAlias = str
+"""特徴ラベル"""
