656ccebb95a7febaa4e17489b64cba5a77c4b216
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2024-01-13 17:59:26 +0000

    📝 Add docstring of regular expression pattern for strings representing numbers

--- authorship_tool/util/feature/regex.py
+++ authorship_tool/util/feature/regex.py
@@ -8,3 +8,8 @@ import re
 NUMERIC_VALUE_PATTERN: re.Pattern[str] = re.compile(
     r"[+-]?(?:\d+\.?\d*|\.\d+)(?:(?:[eE][+-]?\d+)|(?:\*10\^[+-]?\d+))"
 )
+"""
+数値を表す文字列の正規表現パターン
+Regular expression pattern for strings representing numbers
+e.g.) 1.23, 1.23e+4, 1.23*10^4, -1.23e+4
+"""
