fb40ff1fca56d02e33b63af851942e2bd6ed8751
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2024-01-14 07:07:08 +0000

    🔧 Update paths in config module

--- authorship_tool/config.py
+++ authorship_tool/config.py
@@ -1,6 +1,11 @@
-PATHS = {
-    "path_dump_lgbm_model_dir": "dump/lgbm/model",
+"""設定モジュール(Configuration Module))"""
+
+from authorship_tool.types import EnvKey, PathStr
+
+PATHS: dict[EnvKey, PathStr] = {
+    "path_adjective_past_participle_dataset": "data/john_blake_2023/wordLists/adjectivesPastParticiple",
+    "path_text_data_dir": "dump/text_data",
     "path_dump_dataset_dir": "dump/dataset",
+    "path_dump_lgbm_model_dir": "dump/lgbm/model",
     "path_dump_shap_figure_dir": "dump/shap/figure",
-    "path_adjective_past_participle_dataset": "data/nlp_datasets/wordLists/adjectivesPastParticiple",
 }
