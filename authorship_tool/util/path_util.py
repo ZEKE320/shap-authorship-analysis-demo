797bb2c1b359143aecf378f37d337253a56bf436
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2024-01-14 07:09:34 +0000

    📝 Add path initialization and documentation

--- authorship_tool/util/path_util.py
+++ authorship_tool/util/path_util.py
@@ -1,3 +1,4 @@
+"""パスユーティリティモジュール (Path utility module)"""
 import os
 from pathlib import Path
 
@@ -5,24 +6,44 @@ from authorship_tool.config import PATHS
 
 
 class PathUtil:
+    """パスユーティリティクラス (Path utility class)"""
+
     PROJECT_ROOT: Path
-    LGBM_MODEL_DIR: Path
+    TEXT_DATA_DIR: Path
     DATASET_DIR: Path
-    SHAP_FIGURE_DIR: Path
     PAST_PARTICIPLE_ADJECTIVE_DATASET: Path
+    LGBM_MODEL_DIR: Path
+    SHAP_FIGURE_DIR: Path
 
     @classmethod
     def initialize_all_paths(cls) -> None:
+        """
+        環境変数からパスを初期化する
+        Initialize all paths from environment variables
+        """
+
         cls.PROJECT_ROOT = cls.__initialize_project_root()
-        cls.LGBM_MODEL_DIR = cls.__initialize_path("path_dump_lgbm_model_dir")
+        cls.TEXT_DATA_DIR = cls.__initialize_path("path_text_data_dir")
         cls.DATASET_DIR = cls.__initialize_path("path_dump_dataset_dir")
-        cls.SHAP_FIGURE_DIR = cls.__initialize_path("path_dump_shap_figure_dir")
         cls.PAST_PARTICIPLE_ADJECTIVE_DATASET = cls.__initialize_path(
             "path_adjective_past_participle_dataset"
         )
+        cls.LGBM_MODEL_DIR = cls.__initialize_path("path_dump_lgbm_model_dir")
+        cls.SHAP_FIGURE_DIR = cls.__initialize_path("path_dump_shap_figure_dir")
 
     @staticmethod
     def __initialize_project_root() -> Path:
+        """
+        プロジェクトのルートディレクトリを初期化する
+        Initialize the project root directory
+
+        Raises:
+            ValueError: pyproject.tomlが見つからない場合 (If pyproject.toml is not found)
+
+        Returns:
+            Path: プロジェクトのルートディレクトリ (The project root directory)
+        """
+
         file_dir: Path = Path(os.path.dirname("__file__")).resolve()
 
         for directory in [*file_dir.parents, file_dir]:
@@ -34,6 +55,22 @@ class PathUtil:
 
     @classmethod
     def __initialize_path(cls, env_key: str) -> Path:
+        """
+        環境変数からパスを初期化する
+        Initialize the path from the environment variable
+
+        Args:
+            env_key (str): 環境変数のキー (Environment variable key)
+
+        Raises:
+            ValueError: PROJECT_ROOT_PATHが初期化されていない場合 (If PROJECT_ROOT_PATH is not initialized)
+            ValueError: 環境変数が見つからない場合 (If the environment variable is not found)
+            FileNotFoundError: ファイルが見つからない場合 (If the file is not found)
+
+        Returns:
+            Path: パス (Path)
+        """
+
         if cls.PROJECT_ROOT is None:
             raise ValueError("Path: `PROJECT_ROOT_PATH` is not initialized.")
 
