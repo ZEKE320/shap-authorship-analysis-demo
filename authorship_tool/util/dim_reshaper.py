7f110a48812f88709415784afd27522ac8367e65
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2024-01-14 22:36:57 +0000

    ♻️ Refactor dim_reshaper.py to use more descriptive variable names

--- authorship_tool/util/dim_reshaper.py
+++ authorship_tool/util/dim_reshaper.py
@@ -1,12 +1,11 @@
 """配列の次元を変換するモジュール"""
 
-
 from authorship_tool.types_ import Para2dStr, Sent1dStr
 
 
-def reduce_dim(para: Para2dStr, /) -> Sent1dStr:
+def reduce_dim(two_dim_str: list | set | tuple, /) -> list:
     """段落のリストを文章のリストに変換する"""
-    return [word for sent in para for word in sent]
+    return [single_str for one_dim_str in two_dim_str for single_str in one_dim_str]
 
 
 def one_dim_to_str(sent: Sent1dStr, /) -> str:
