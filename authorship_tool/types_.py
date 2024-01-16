c050ea24313587edc13bd8403656ba01b4a50faa
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2024-01-16 15:41:47 +0000

    ✨ 新たな型ヒントを導入 & 型ヒントのdocstringを改良

--- authorship_tool/types_.py
+++ authorship_tool/types_.py
@@ -4,32 +4,38 @@ from typing import TypeAlias
 # 文章関連
 
 Tag: TypeAlias = str
-"""タグ"""
+"""タグ (Tag)"""
 
 TokenStr: TypeAlias = str
-"""単語"""
+"""単語 (Token)"""
 
 Sent1dStr: TypeAlias = list[TokenStr]
-"""文"""
+"""文 (Sentence)"""
 
 Para2dStr: TypeAlias = list[Sent1dStr]
-"""段落"""
+"""段落 (Paragraph)"""
+
+Docs3dStr: TypeAlias = list[Para2dStr]
+"""文書 (Document)"""
+
+AuthorCollection4dStr: TypeAlias = list[Docs3dStr]
+"""1人の著者に関連する文書の集合 (Author's document collection)"""
 
 TaggedToken: TypeAlias = tuple[TokenStr, Tag]
-"""タグ付けされた単語"""
+"""タグ付けされた単語 (Tagged tokens)"""
 
 # 特徴量関連
 
 FeatureLabel: TypeAlias = str
-"""特徴ラベル"""
+"""特徴ラベル (Feature label)"""
 
 Char: TypeAlias = str
-"""文字"""
+"""文字 (Character)"""
 
 # 環境変数
 
 EnvKey: TypeAlias = str
-"""環境変数のキー"""
+"""環境変数のキー (Environment variable key)"""
 
 PathStr: TypeAlias = str
-"""パス文字列"""
+"""パス文字列 (Path string)"""
