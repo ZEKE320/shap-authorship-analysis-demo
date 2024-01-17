4a5e56b49c44412f96bbe93bdb2820045ae6f4a1
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2024-01-17 00:07:09 +0000

    🏷️ 型ヒントを追加

--- authorship_tool/types_.py
+++ authorship_tool/types_.py
@@ -10,16 +10,28 @@ TokenStr: TypeAlias = str
 """単語 (Token)"""
 
 Sent1dStr: TypeAlias = list[TokenStr]
-"""文 (Sentence)"""
+"""文 (Sentence) - list[str]"""
+
+Sentence: TypeAlias = str
+"""文 (Sentence) - str"""
 
 Para2dStr: TypeAlias = list[Sent1dStr]
-"""段落 (Paragraph)"""
+"""段落 (Paragraph) - list[list[str]]"""
 
-Docs3dStr: TypeAlias = list[Para2dStr]
-"""文書 (Document)"""
+Paragraph: TypeAlias = str
+"""段落 (Paragraph) - str"""
 
-AuthorCollection4dStr: TypeAlias = list[Docs3dStr]
-"""1人の著者に関連する文書の集合 (Author's document collection)"""
+Doc3dStr: TypeAlias = list[Para2dStr]
+"""文書 (Document) - list[list[list[str]]]"""
+
+Document: TypeAlias = str | list[Paragraph]
+"""文書 (Document) - str"""
+
+AuthorColl4dStr: TypeAlias = list[Doc3dStr]
+"""1人の著者に関連する文書の集合 (Author's document collection) - list[list[list[list[str]]]]"""
+
+AuthorCollection: TypeAlias = str | list[Document]
+"""1人の著者に関連する文書の集合 (Author's document collection) - str"""
 
 TaggedToken: TypeAlias = tuple[TokenStr, Tag]
 """タグ付けされた単語 (Tagged tokens)"""
